ad3ae47334941a5123b103ddc247ad35f7b8c372
rohitsnegi1450 <rohitsinghnegi977@gmail.com>
2021-04-23 17:16:25 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <table>
+                    <tr>
+                        <td><label>First Name</label></td>
+                        <tr> <td><input type="text" /></td></tr>
+
+                        <td><label>Last Name</label></td>
+                        <td><input type="text" /></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    )
+}
+export default Login

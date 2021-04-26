0ad1d8cd9585b946521f54b7338863ecd4576402
Mayank Kumawat <mayankkumawat015@gmail.com>
2021-04-26 16:08:08 +0530

    designed SignUp Page

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,5 +1,6 @@
 import device from '../Images/device.png'
 import icon1 from '../Images/trial-icon-1.png'
+import logo from '../Images/salesforce-logo.png'
 
 function SignupPage() {
     return (
@@ -8,53 +9,70 @@ function SignupPage() {
 
                 {/* head-info */}
 
-                <div className="head">
-                    <h2 className="headfirst">Get Your FREE 30-Days Trial Now!</h2>
-                    <h3 className="headsecond">Sales Cloud - Professional Edition</h3>
-                    <p className="headpara">Experience why over 2 million customers choose Salesforce CRM</p>
-                    <img src={device} width="250px" />
+                <div className="head-div container-fluid">
+                    <div className="d-flex">
+                        <div className="logo">
+                            <img src={logo} />
+                        </div>
+                        <div className="head">
+                            <span className="headfirst">Get Your FREE 30-Days Trial Now!</span><br />
+                            <span className="headsecond">Sales Cloud - Professional Edition</span><br />
+                            <span className="headpara">Experience why over 2 million customers choose Salesforce CRM</span>
+                        </div>
+                    </div>
+                    <div className="main-img mt-3">
+                        <img src={device} width="373px" />
+                    </div>
                 </div>
 
                 {/* features */}
 
                 <div className="features">
-                    <div className="row">
-                        <div className="col-md-3">
+                    <div className="row justify-content-center">
+                        <div className="col-md-6 mt-4">
                             <div className="icons-info d-flex">
                                 <div className="icon">
                                     <img src={icon1} />
                                 </div>
-                                <p>Absolutely FREE</p>
-                                <p>No hidden charges, No credit card required</p>
+                                <div className="features-head" >
+                                    <div className="features-heading flex-column">Absolutely FREE</div>
+                                    <p className="features-details">No hidden charges, No credit card required</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-6 mt-4">
                             <div className="icons-info d-flex">
                                 <div className="icon">
                                     <img src={icon1} />
                                 </div>
-                                <p>Absolutely FREE</p>
-                                <p>No hidden charges, No credit card required</p>
+                                <div className="features-head">
+                                    <div className="features-heading flex-column">Unlimited Features</div>
+                                    <p className="features-details">Access all features of the world's #1 CRM!</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-3">
+                    <div className="row justify-content-center">
+                        <div className="col-md-6 mt-4">
                             <div className="icons-info d-flex">
                                 <div className="icon">
                                     <img src={icon1} />
                                 </div>
-                                <p>Absolutely FREE</p>
-                                <p>No hidden charges, No credit card required</p>
+                                <div className="features-head">
+                                    <div className="features-heading flex-column">Fast & Easy</div>
+                                    <p className="features-details">Get access instantly, no downloads required</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-6 mt-4">
                             <div className="icons-info d-flex">
                                 <div className="icon">
                                     <img src={icon1} />
                                 </div>
-                                <p>Absolutely FREE</p>
-                                <p>No hidden charges, No credit card required</p>
+                                <div className="features-head">
+                                    <div className="features-heading flex-column">Your Own Data</div>
+                                    <p className="features-details">Enjoy the Free Trial with your company data</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -62,13 +80,14 @@ function SignupPage() {
 
                 {/* contact */}
 
-                <div className="contact">
-                    <p>DO YOU PREFER TALKING TO A REPRESENTATIVE?</p>
+                <div className="contact text-center mt-5">
+                    <span className="contact-span">DO YOU PREFER TALKING TO A REPRESENTATIVE?</span>
                     <p>Call us at <b>000 800 001 6000</b></p>
                 </div>
             </div>
             <div className="col-md-6"></div>
         </div>
+
     )
 
 }

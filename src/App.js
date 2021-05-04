2aaa5fe65f071b0a7e15fddb596109b03c3dc175
Mayank Kumawat <mayankkumawat015@gmail.com>
2021-05-04 11:09:08 +0530

    sidebar nd navbar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import SignupPage from './components/SignupPage';
 import LoginPage from './components/LoginPage';
+import ForgetPassword from './components/ForgetPassPage';
+import Main from './components/Main';
+import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
-
+      {/* <LoginPage /> */}
+      {/* <ForgetPassword /> */}
+      <CssBaseline />
+      <Main />
     </div>
   );
 }
